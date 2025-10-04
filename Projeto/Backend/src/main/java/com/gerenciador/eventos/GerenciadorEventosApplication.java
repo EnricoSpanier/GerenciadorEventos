de24ac235bb82dddddf1d49e40d587a694a7af51
EnricoSpanier <enricospanier@gmail.com>
2025-10-03 23:42:40 -0300

    Refactor backend project structure and update class files
    - Updated EventWallet.class and User.class with new implementations.
    - Added new GerenciadorEventosApplication.class to serve as the main application entry point.
    - Updated pom.properties to reflect the new artifactId, groupId, and version.
    - Modified input and created files lists for the maven-compiler-plugin to include new and updated classes.
    - Removed outdated surefire reports and dumpstream files to clean up the target directory.

---
+++ Projeto/Backend/src/main/java/com/gerenciador/eventos/GerenciadorEventosApplication.java
@@ -0,0 +1,12 @@
+package com.gerenciador.eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorEventosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorEventosApplication.class, args);
+    }
+}
