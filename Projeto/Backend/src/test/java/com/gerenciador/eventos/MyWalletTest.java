f2a4aa60921f4ddad9d84ad1e8705384277ed962
Guilherme <72773527+endydragonn@users.noreply.github.com>
2025-10-10 12:30:07 -0300

    Update MyWalletTest.java

--- Projeto/Backend/src/test/java/com/gerenciador/eventos/MyWalletTest.java
+++ Projeto/Backend/src/test/java/com/gerenciador/eventos/MyWalletTest.java
@@ -1,5 +1,28 @@
-package com.gerenciador.eventos;
+/*package com.gerenciador.eventos;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 
 public class MyWalletTest {
     
+    public MyWalletTest() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    MyWallet objMyWallet = new MyWallet();
+
+    //testando os Getters
+
+    @Test
+    public void testGetUserId() {
+        objMyWallet.setUserId(1L);
+        assertEquals(1L, objMyWallet.getUserId());
+    }
+
 }
+*/
