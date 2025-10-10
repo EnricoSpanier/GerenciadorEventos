166181a4f2e48b26f6894fd77420b3b6c6392704
Guilherme <72773527+endydragonn@users.noreply.github.com>
2025-10-10 12:29:52 -0300

    Update EventWalletTest.java

--- Projeto/Backend/src/test/java/com/gerenciador/eventos/EventWalletTest.java
+++ Projeto/Backend/src/test/java/com/gerenciador/eventos/EventWalletTest.java
@@ -1,5 +1,33 @@
 package com.gerenciador.eventos;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+
 public class EventWalletTest {
     
+    public EventWalletTest() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    EventWallet objEventWallet = new EventWallet();
+
+    //testando os Getters
+
+    @Test
+    public void testGetUserId() {
+        objEventWallet.setUserId(1L);
+        assertEquals(1L, objEventWallet.getUserId());
+    }
+
+    @Test
+    public void testGetEventId() {
+        objEventWallet.setEventId(1L);
+        assertEquals(1L, objEventWallet.getEventId());
+    }
+
 }
